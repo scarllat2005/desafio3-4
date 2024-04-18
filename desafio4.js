c64780c07457720471143bded3e10cb626ec86b8
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-04-18 08:13:59 -0300

    Add files via upload

---
+++ desafio4.js
@@ -0,0 +1,32 @@
+const aluno =[
+"João",//0
+"Juliana",//1
+"Ana",//2
+"Caio",//3
+"Lara",//4
+"Marjorie",//5
+"Guilherme",//6
+"Aline",//7
+"Fabiana",//8
+"Andre",//9
+"Carlos",//10
+"Paulo",
+"Bia",
+"Vivian",
+"Isabela",
+"Vinícius",
+"Renan",
+"Renata",
+"Daisy",
+"Camilo" 
+]
+
+//const sala1 = alunos.slice(0,10)
+//const sala2 = alunos.slice(10)
+const salal = alunos.slice(0,alunos.length/2)
+const sala2 = alunos.slice(alunos.length/2)
+
+console.log(sala1)
+console.log(sala2)
+
+//console.log(alunos)
